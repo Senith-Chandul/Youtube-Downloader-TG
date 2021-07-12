4a999438a299bb57f47ce31342b7fa72811df187
Senith-Chandul <79186863+Senith-Chandul@users.noreply.github.com>
2021-07-12 19:06:21 +0530

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
-@Client.on_message(Filters.command(["tute"]))
+@Client.on_message(Filters.command(["bot"]))
 async def start(client, message):
-    await message.reply_text("To make a YouTube downloader pess ⚜️ Go ⚜️ button to go youtube directly. 🥳",
+    await message.reply_text("Url Uploader Bot",
                              reply_markup=InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("⚜️ Go ⚜️", url="https://youtu.be/l6rTppg3a1w")]
+            [InlineKeyboardButton("⚜️ Go ⚜️", url="https://t.me/SenithUrlUploaderBot")]
         ]))
     
